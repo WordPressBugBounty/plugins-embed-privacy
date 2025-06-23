d8c882716a71f278166125a3fa59824c9aff48bd
Sean Murphy <sean@iamseanmurphy.com>
2025-06-23 13:24:38 +0000

    1.11.1

--- embed-privacy/embed-privacy.php
+++ embed-privacy/embed-privacy.php
@@ -5,7 +5,7 @@ namespace epiphyt\Embed_Privacy;
 Plugin Name:		Embed Privacy
 Plugin URL:			https://epiph.yt/en/embed-privacy/
 Description:		Embed Privacy prevents from loading external embeds directly and lets the user control which one should be loaded.
-Version:			1.11.0
+Version:			1.11.1
 Author:				Epiphyt
 Author URI:			https://epiph.yt/en/
 License:			GPL2
@@ -30,20 +30,21 @@ along with Embed Privacy. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 \defined( 'ABSPATH' ) || exit;
 
-\define( 'EMBED_PRIVACY_VERSION', '1.11.0' );
+if ( ! \defined( 'EPI_EMBED_PRIVACY_BASE' ) ) {
+	if ( \file_exists( \WP_PLUGIN_DIR . '/embed-privacy/' ) ) {
+		\define( 'EPI_EMBED_PRIVACY_BASE', \WP_PLUGIN_DIR . '/embed-privacy/' );
+	}
+	else if ( \file_exists( \WPMU_PLUGIN_DIR . '/embed-privacy/' ) ) {
+		\define( 'EPI_EMBED_PRIVACY_BASE', \WPMU_PLUGIN_DIR . '/embed-privacy/' );
+	}
+	else {
+		\define( 'EPI_EMBED_PRIVACY_BASE', \plugin_dir_path( __FILE__ ) );
+	}
+}
 
-if ( \file_exists( \WP_PLUGIN_DIR . '/embed-privacy/' ) ) {
-	\define( 'EPI_EMBED_PRIVACY_BASE', \WP_PLUGIN_DIR . '/embed-privacy/' );
-}
-else if ( \file_exists( \WPMU_PLUGIN_DIR . '/embed-privacy/' ) ) {
-	\define( 'EPI_EMBED_PRIVACY_BASE', \WPMU_PLUGIN_DIR . '/embed-privacy/' );
-}
-else {
-	\define( 'EPI_EMBED_PRIVACY_BASE', \plugin_dir_path( __FILE__ ) );
-}
-
-\define( 'EPI_EMBED_PRIVACY_FILE', __FILE__ );
-\define( 'EPI_EMBED_PRIVACY_URL', \plugin_dir_url( \EPI_EMBED_PRIVACY_BASE . 'embed-privacy.php' ) );
+\define( 'EPI_EMBED_PRIVACY_FILE', \EPI_EMBED_PRIVACY_BASE . \basename( __FILE__ ) );
+\define( 'EPI_EMBED_PRIVACY_URL', \plugin_dir_url( \EPI_EMBED_PRIVACY_FILE ) );
+\define( 'EMBED_PRIVACY_VERSION', '1.11.1' );
 
 if ( ! \class_exists( 'DOMDocument' ) ) {
 	/**
@@ -69,6 +70,11 @@ if ( ! \class_exists( 'DOMDocument' ) ) {
 \spl_autoload_register( static function( $class_name ) {
 	$path = \explode( '\\', $class_name );
 	$filename = \str_replace( '_', '-', \strtolower( \array_pop( $path ) ) );
+	
+	if ( \strpos( $class_name, __NAMESPACE__ ) !== 0 ) {
+		return;
+	}
+	
 	$class_name = \str_replace(
 		[ 'epiphyt\embed_privacy\\', '\\', '_' ],
 		[ '', '/', '-' ],
